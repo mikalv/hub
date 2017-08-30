d872f92cc925753def92793559013616b6e70d4a
Chris Dukakis <chrisdukakis@gmail.com>
2017-08-30 19:22:32 +0300

    replace async/await with promises

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = {
-  'getAddress'          : require('./lib/address').getAddress,
-  'process'             : require('./lib/process').process,
-  'sweep'               : require('./lib/process').sweep,
-  'updateBalances'      : require('./lib/process').updateBalances,
-  'replaySweeps'        : require('./lib/process').replaySweeps,
-  'getUnspentBalance'   : require('./lib/process').getUnspentBalance,
-  'sync'                : require('./lib/sync'),
-  'transfer'            : require('./lib/transfer').tranfer,
-  'getPendingTransfers' : require('./lib/transfer').getPendingTransfers,
-  'replayTransfer'      : require('./lib/transfer').replayTransfer
+  'getAddress'              : require('./lib/address').getAddress,
+  'process'                 : require('./lib/process').process,
+  'sweep'                   : require('./lib/process').sweep,
+  'updateBalances'          : require('./lib/process').updateBalances,
+  'replaySweeps'            : require('./lib/process').replaySweeps,
+  'getUnspentBalance'       : require('./lib/process').getUnspentBalance,
+  'getAddressesWithBalance' : require('./lib/process').getAddressesWithBalance,
+  'sync'                    : require('./lib/sync'),
+  'transfer'                : require('./lib/transfer').tranfer,
+  'getPendingTransfers'     : require('./lib/transfer').getPendingTransfers,
+  'replayTransfer'          : require('./lib/transfer').replayTransfer
 };
