dffacd028608aaf31b4f6f27d2dccd5fad2e4c1b
Chris Dukakis <chrisdukakis@gmail.com>
2017-09-01 21:22:08 +0300

    implement transfer, return the trytes in sweep & transfer

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 module.exports = {
   'getAddress'              : require('./lib/address').getAddress,
+  'isSpent'                 : require('./lib/address').isSpent,
   'process'                 : require('./lib/process').process,
   'sweep'                   : require('./lib/process').sweep,
   'updateBalances'          : require('./lib/process').updateBalances,
-  'replay'                  : require('./lib/transfer').replay,
   'getUnspentBalance'       : require('./lib/process').getUnspentBalance,
   'getAddressesWithBalance' : require('./lib/process').getAddressesWithBalance,
   'sync'                    : require('./lib/sync').sync,
   'getPendingTrasnactions'  : require('./lib/sync').getPendingTransactions,
+  'getConfirmedTrasnactions': require('./lib/sync').getConfirmedTransactions,
   'transfer'                : require('./lib/transfer').transfer,
-  'getInputs'               : require('./lib/transfer').getInputs
+  'sendTrytes'              : require('./lib/transfer').sendTrytes,
+  'getInputs'               : require('./lib/transfer').getInputs,
+  'getInputWithLowestValue' : require('./lib/').getInputWithLowestValue,
+  'replay'                  : require('./lib/transfer').replay,
+  'isReattachable'          : require('./lib/transfer').isReattachable
 };
