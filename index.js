673c915d25a5d4306de26c297f015cba9e4e9fb6
Andy Wermke <andy@dev.next-step-software.com>
2017-09-03 11:54:32 +0200

    Just some non-functional changes
    We really need to add some tests. The `getInputWithLowestValue()` was broken before and noone noticed.

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+const { getAddress, isSpent } = require('./lib/address')
+const { getAddressesWithBalance, getUnspentBalance, process, sweep } = require('./lib/process')
+const { getConfirmedTransactions, getPendingTransactions, sync, updateBalances } = require('./lib/sync')
+const { getInputs, getInputWithLowestValue, isReattachable, replay, sendTrytes, transfer } = require('./lib/transfer')
+
 module.exports = {
-  'getAddress': require('./lib/address').getAddress,
-  'isSpent': require('./lib/address').isSpent,
-  'process': require('./lib/process').process,
-  'sweep': require('./lib/process').sweep,
-  'getUnspentBalance': require('./lib/process').getUnspentBalance,
-  'getAddressesWithBalance': require('./lib/process').getAddressesWithBalance,
-  'sync': require('./lib/sync').sync,
-  'getPendingTransactions': require('./lib/sync').getPendingTransactions,
-  'getConfirmedTransactions': require('./lib/sync').getConfirmedTransactions,
-  'updateBalances': require('./lib/sync').updateBalances,
-  'transfer': require('./lib/transfer').transfer,
-  'sendTrytes': require('./lib/transfer').sendTrytes,
-  'getInputs': require('./lib/transfer').getInputs,
-  'getInputWithLowestValue': require('./lib/').getInputWithLowestValue,
-  'replay': require('./lib/transfer').replay,
-  'isReattachable': require('./lib/transfer').isReattachable
+  getAddress,
+  isSpent,
+  process,
+  sweep,
+  getUnspentBalance,
+  getAddressesWithBalance,
+  sync,
+  getPendingTransactions,
+  getConfirmedTransactions,
+  updateBalances,
+  transfer,
+  sendTrytes,
+  getInputs,
+  getInputWithLowestValue,
+  replay,
+  isReattachable
 }
